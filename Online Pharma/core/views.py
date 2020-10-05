2ed65e295ef9b2e967e51c3154b9323c9ec08e40
Kashish Jain <kashishjain32@gmail.com>
2020-10-05 22:33:44 +0530

    Few changes

--- Online Pharma/core/views.py
+++ Online Pharma/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Medicine
 
 
-def item_list():
-    pass
+def item_list(request):
+    context = {
+        'medicines': Medicine.objects.all()
+    }
+    return render(request, "home-page.html", context)
