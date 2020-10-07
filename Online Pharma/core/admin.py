6f38ba1b63b8c32380dabe517d04b61e3d801aa1
kashishvjain <66744312+kashishvjain@users.noreply.github.com>
2020-10-08 03:32:32 +0530

    Changed a little ui

--- Online Pharma/core/admin.py
+++ Online Pharma/core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import OrderItem,Medicine,Order
+from .models import Medicine,Order
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Medicine)
-admin.site.register(OrderItem)
--- Online Pharma/core/views.py
+++ Online Pharma/core/views.py
@@ -4,6 +4,6 @@ from .models import Medicine
 
 def item_list(request):
     context = {
-        'medicines': Medicine.objects.all()
+        
     }
     return render(request, "home-page.html", context)
