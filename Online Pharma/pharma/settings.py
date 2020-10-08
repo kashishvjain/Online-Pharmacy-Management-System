55fae59ac48d1657755099f42ddd5491a1eb90f3
kashishvjain <66744312+kashishvjain@users.noreply.github.com>
2020-10-08 14:55:03 +0530

    Login remake

--- Online Pharma/pharma/settings.py
+++ Online Pharma/pharma/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -88,3 +89,6 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
